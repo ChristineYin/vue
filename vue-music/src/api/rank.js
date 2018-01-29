07c5914de7204d03de9df360d3122be94c356e92
FAREAST\v-chuyin <v-chuyin@microsoft.com>
2018-01-29 14:58:57 +0800

    vue 音乐webapp

---
+++ vue-music/src/api/rank.js
@@ -0,0 +1,37 @@
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from './config.js'
+// import axios from 'axios'
+
+export function getTopList () {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    uin: 0,
+    needNewCode: 1,
+    platform: 'h5'
+  })
+
+  return jsonp(url, data, options)
+}
+
+export function getMusicList (topid) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    tpl: 3,
+    page: 'detail',
+    topid,
+    type: 'top',
+    song_begin: 0,
+    song_num: 30,
+    g_tk: 1871187710,
+    format: 'jsonp',
+    inCharset: 'utf8',
+    outCharset: 'utf-8',
+    notice: 0,
+    platform: 'yqq',
+    needNewCode: 0
+  })
+
+  return jsonp(url, data, options)
+}
---
+++ vue-music/src/router/index.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Rank from 'components/rank/rank'
+import Recommend from 'components/recommend/recommend'
+import Search from 'components/search/search'
+import Singer from 'components/singer/singer'
+import SingerDetail from 'components/singer-detail/singer-detail'
+import Disc from 'components/disc/disc'
+import TopList from 'components/top-list/top-list'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/recommend'
+    },
+    {
+      path: '/rank',
+      name: 'Rank',
+      component: Rank,
+      children: [
+        {
+          path: ':id',
+          component: TopList
+        }
+      ]
+    },
+    {
+      path: '/recommend',
+      name: 'Recommend',
+      component: Recommend,
+      children: [
+        {
+          path: ':id',
+          component: Disc
+        }
+      ]
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search
+    },
+    {
+      path: '/singer',
+      name: 'Singer',
+      component: Singer,
+      children: [
+        {
+          path: ':id',
+          component: SingerDetail
+        }
+      ]
+    }
+  ]
+})
